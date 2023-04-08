0d494b52c2c055dabe0d0aa1263061bea25b8877
NikitaDobry27 <bazhenov.nikita27@gmail.com>
2023-04-08 17:55:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 
 import re
 from collections import UserDict
-from datetime import datetime
+import datetime
+from itertools import islice
+from sys import maxsize
+import json
 
 
 class Field:
@@ -31,38 +34,48 @@ class Phone(Field):
         self._value = new_value
 
     def validate_phone(self, value):
-        phone_pattern = re.compile(r'^\+?[\d\s\-()]{9,15}$')
+        phone_pattern = re.compile(r'^\+?[\d\s()]{9,15}$')
         if not phone_pattern.match(value):
             raise ValueError("Invalid phone number format")
         return value
 
 class Birthday(Field):
-    def __init__(self, value=None):
-        if value:
-            value = self.validate_birthday(value)
-        super().__init__(value)
+    def __init__(self, value):
+        self.value = value
 
     @property
     def value(self):
         return self._value
-
+    
     @value.setter
-    def value(self, new_value):
-        if new_value:
-            new_value = self.validate_birthday(new_value)
-        self._value = new_value
+    def value(self, value):
+        if value is None:
+            self._value = value
+        else:
+            self._value = self.date_validation(value)
 
-    def validate_birthday(self, value):
-        try:
-            value = datetime.strptime(str(value), '%d.%m.%Y').date()
-            return value
-        except ValueError:
-            raise ValueError("Invalid date format. Please use 'dd.mm.yyyy'")
+    def date_validation(self, bday):
+        
+        date_types = ["%d.%m.%Y", '%d.%m']
+        for date_type in date_types:
+            try:
+                self._value = datetime.datetime.strptime(str(bday), date_type).date()
+                return self._value
+            except ValueError:
+                pass
+        raise TypeError(f"{bday}Incorrect date format, should be dd.mm.yyyy or dd.mm")
 
+    def __str__(self):
+        if self._value.year == 1900:
+            return self._value.strftime("%d.%m")
+        else:
+            return self._value.strftime("%d.%m.%Y")
+        
 class Record:
     def __init__(self, name, phones=None, birthday=None):
         self.name = Name(name)
         self.phones = []
+        self.birthday = None
         if birthday:
             self.birthday = Birthday(birthday)
         else:
@@ -71,36 +84,85 @@ class Record:
             for phone in phones:
                 self.add_phone(phone)
 
+    def has_phone(self, phone):
+        for existing_phone in self.phones:
+            if existing_phone.value == phone.value:
+                return True
+        return False
+
     def add_phone(self, phone):
-        self.phones.append(phone)
+        if not self.has_phone(phone):
+            self.phones.append(phone)
+        else:
+            print("Phone number is already added for this contact.")
         
     def clear_phones(self):
         self.phones.clear()
-
+    
     def days_to_birthday(self):
         if not self.birthday:
-            return None
-        today = datetime.date.today()
-        birthday_date = self.birthday.value.day
-        birthday_month = self.birthday.value.month
-        new_birthday = datetime.date(today.year, birthday_month, birthday_date)
-        
-        if new_birthday < today:
-            new_birthday = datetime.date(today.year + 1, birthday_month, birthday_date)
+            return f"No birthday added to contact {self.name.value}"
+        today = datetime.datetime.now().date()
+        birthday_date = datetime.date(today.year, self.birthday.value.month, self.birthday.value.day)
+        days_to_birt = (birthday_date - today).days
+        if days_to_birt < 0:
+            birthday_date = datetime.date(today.year + 1, self.birthday.value.month, self.birthday.value.day)
+            days_to_birt = (birthday_date - today).days
+        if days_to_birt == 0:
+            return f"Sing for {self.name.value} a song, cuz' {self.name.value}'s b-day is today! "
+        return days_to_birt
 
-        time_to_birthday = new_birthday - today
-        return time_to_birthday.days
 
     def __str__(self) -> str:
         phones_str = ', '.join([str(phone) for phone in self.phones])
+        if self.birthday:
+            return f"{self.name}: {phones_str}: {self.birthday}"
         return f"{self.name}: {phones_str}"
                 
 
 class AddressBook(UserDict):
-        
+
+    def save_to_file(self, file_name):
+        data_to_save = {
+            name: {
+                'name': record.name.value,
+                'phones': [phone.value for phone in record.phones],
+                'birthday': record.birthday.value.strftime('%d.%m.%Y') if record.birthday else None
+            } for name, record in self.data.items()
+        }
+
+        with open(file_name, 'w') as file:
+            json.dump(data_to_save, file)
+
+    def load_from_file(self, file_name):
+        try:
+            with open(file_name, 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            print(f"File {file_name} not found. Creating a new file.")
+            with open(file_name, 'w') as file:
+                json.dump({}, file)
+            data = {}
+
+        for name, record_data in data.items():
+            record = Record(
+                name=record_data['name'],
+                phones=[Phone(phone) for phone in record_data['phones']],
+                birthday=record_data['birthday']
+            )
+            self.data[name] = record
+
+    def __iter__(self, num_records=None):
+        iterator = iter(self.data.values())
+        while True:
+            page = list(islice(iterator, num_records or maxsize))
+            if not page:
+                break
+            yield page
+
     def add_record(self, record):
         self.data[record.name.value] = record
-        
+        self.save_to_file("contacts.json")
         
     def find_records(self, query):
         result = []
@@ -113,9 +175,9 @@ class AddressBook(UserDict):
 contacts = AddressBook()
 
 def input_validation(func):
-    def wrapper(*args):
+    def wrapper(*args, **kwargs):
         try:
-            return func(*args)
+            return func(*args, **kwargs)
         except IndexError:
             return '\nPlease provide name and phone number\n'
         except KeyError:
@@ -125,13 +187,14 @@ def input_validation(func):
     return wrapper
 
 
-def input_formatter(str):
-    words_from_string = re.findall(r'\b\w+\b', str)
+def input_formatter(input_str):
+    words_from_string = input_str.split()
     command = words_from_string[0]
 
     if len(words_from_string) == 1:
-        return command, ''
-    data = words_from_string[1:]
+        return command, ()
+    
+    data = tuple(words_from_string[1:])
     return command, data
 
 def command_handler(command):
@@ -152,18 +215,31 @@ def hello(*args):
 def add_contact(*args):
     lst = args[0]
     name = lst[0]
-    phone = lst[1]
-    birthday = '.'.join(lst[2:])
 
     if len(lst) < 2:
         raise IndexError
-    if len(phone) > 15 or len(phone) < 9:
-        return f'\n{phone} is not valid phone number\n'
-    
-    record = Record(name, phones=[phone], birthday=birthday)
+
+    phone_obj = Phone(lst[1])
+
+    if len(lst) == 3:
+        birthday = Birthday(lst[2])
+    else:
+        birthday = None
+
+    existing_records = contacts.find_records(name)
+    if existing_records:
+        for record in existing_records:
+            if not record.has_phone(phone_obj):
+                record.add_phone(phone_obj)
+                return f'\nNew phone number was added to contact {name.capitalize()}\n'
+            else:
+                return f'\nThe phone number is already added to contact {name.capitalize()}\n'
+
+    record = Record(name, phones=[phone_obj], birthday=birthday)
     contacts.add_record(record)
    
     return f'\nContact {name.capitalize()} was successfully added\n'
+
 
 
 @input_validation
@@ -183,7 +259,7 @@ def change(*args):
         for record in records:
             record.clear_phones()
             record.add_phone(Phone(phone))
-
+        contacts.save_to_file("contacts.json")
         return f'\nPhone number for contact {name.capitalize()} was successfully changed\n'
     else:
         raise KeyError
@@ -228,18 +304,81 @@ def remove_contact(*args):
     if records:
         for record in records:
             contacts.data.pop(record.name.value)
+        contacts.save_to_file("contacts.json")
     else:
         raise KeyError
     return f'\nContact {str(name).capitalize()} was successfully removed\n'
+
+@input_validation
+def days_to_birthday(*args):
+
+    lst = args[0]
+
+    if not lst:
+        return '\nPlease provide "birthday" and contact name\n' 
+    
+    if len(lst) > 1:
+        raise ValueError
+    name = lst[0]
+
+    records = contacts.find_records(name)
+    if not records:
+        return f'\nNo contact with name {name.capitalize()} found\n'
+
+    result = ''
+    for record in records:
+        if record.birthday is not None:
+            days = record.days_to_birthday()
+            result += f'\n{record.name.value.capitalize()} has {days} day(s) left until their birthday.\n'
+        else:
+            result += f'\nNo birthday information found for {record.name.value.capitalize()}\n'
+    return result
+
+def add_birthday(*args):
+    pass
 
 def show_all(*args):
     if not contacts:
         return '\nYour contacts list is empty\n'
     result = ''
-    for record in contacts.values():
+    for record in contacts.data.values():
         name = str(record.name.value).capitalize()
         phones = ', '.join(str(phone) for phone in record.phones)
-        result += f'\n{name}: {phones}\n'
+        birthday = record.birthday.value if record.birthday else None
+        if birthday: 
+            result += f'\n{name}: {phones}: {birthday}\n'
+        else: 
+            result += f'\n{name}: {phones}\n'
+    return result
+
+
+def show_page(*args):
+    page_number, num_records = 1, 2
+    if len(args[0]) > 0:
+        page_number = int(args[0][0])
+        if len(args[0]) > 1:
+            num_records = int(args[0][1])
+
+    if not contacts:
+        return '\nYour contacts list is empty\n'
+    result = ''
+    pages = contacts.__iter__(num_records)
+
+    for _ in range(page_number - 1):
+        next(pages, None)
+    page = next(pages, None)
+
+    if not page:
+        return '\nNo records found on this page\n'
+
+    for record in page:
+        name = str(record.name.value).capitalize()
+        phones = ', '.join(str(phone) for phone in record.phones)
+        birthday = record.birthday.value if record.birthday else None
+        if birthday:
+            result += f'\n{name}: {phones}. Birthday: {birthday}\n'
+        else:
+            result += f'\n{name}: {phones}\n'
     return result
 
 
@@ -249,11 +388,16 @@ COMMANDS = {
     change: ['change'],
     phone: ['phone'],
     show_all: ['show'],
-    remove_contact: ['remove', 'delete']
+    remove_contact: ['remove', 'delete'],
+    show_page: ['page'],
+    days_to_birthday: ['birthday']
 }
 
 
 def main():
+    file_name = 'contacts.json'
+    contacts.load_from_file(file_name)
+
     print(hello())
     while True:
         
@@ -272,7 +416,6 @@ def main():
         call = command_handler(command)
 
         print(call(data))
-
 
 if __name__ == "__main__":
     main()
